71bd3e8877850eb46b633ed7f1dcd84f9190c444
汐洛呀 <xiluo@guimc.ltd>
2022-07-19 00:37:00 +0800

    ---

--- src/test/kotlin/RunMirai.kt
+++ src/test/kotlin/RunMirai.kt
@@ -5,18 +5,15 @@ import net.mamoe.mirai.console.MiraiConsole
 import net.mamoe.mirai.console.plugin.PluginManager.INSTANCE.enable
 import net.mamoe.mirai.console.plugin.PluginManager.INSTANCE.load
 import net.mamoe.mirai.console.terminal.MiraiConsoleTerminalLoader
+import ltd.guimc.lgzbot.plugin.PluginMain
 
 suspend fun main() {
     MiraiConsoleTerminalLoader.startAsDaemon()
 
-    //如果是Kotlin
     PluginMain.load()
     PluginMain.enable()
-    //如果是Java
-//    JavaPluginMain.INSTANCE.load()
-//    JavaPluginMain.INSTANCE.enable()
 
-    val bot = MiraiConsole.addBot(123456, "") {
+    val bot = MiraiConsole.addBot(1324248744, "") {
         fileBasedDeviceInfo()
     }.alsoLogin()
 
