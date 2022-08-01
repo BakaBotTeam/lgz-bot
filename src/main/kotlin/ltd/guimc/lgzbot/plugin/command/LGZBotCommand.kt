6bdaf591644d898d002692e339028585f9038fe2
xiluo <xiluo@guimc.ltd>
2022-08-01 19:38:01 +0800

    Revert "修复汐洛的语文错误+解封atspam+添加unmute命令"
    This reverts commit 33d355eb86ad09164ce0574ab653987a694066be.

--- src/main/kotlin/ltd/guimc/lgzbot/plugin/command/LGZBotCommand.kt
+++ src/main/kotlin/ltd/guimc/lgzbot/plugin/command/LGZBotCommand.kt
@@ -2,7 +2,6 @@ package ltd.guimc.lgzbot.plugin.command
 
 import kotlinx.coroutines.delay
 import ltd.guimc.lgzbot.plugin.PluginMain
-import ltd.guimc.lgzbot.plugin.command.LGZBotCommand.unmute
 import ltd.guimc.lgzbot.plugin.files.Config
 import ltd.guimc.lgzbot.plugin.utils.RandomUtils
 import net.mamoe.mirai.console.command.CommandSender
@@ -12,7 +11,6 @@ import net.mamoe.mirai.console.permission.PermissionId
 import net.mamoe.mirai.console.permission.PermissionService.Companion.hasPermission
 import net.mamoe.mirai.console.permission.PermissionService.Companion.permit
 import net.mamoe.mirai.console.permission.PermitteeId.Companion.permitteeId
-import net.mamoe.mirai.contact.Group
 import net.mamoe.mirai.contact.Member
 import net.mamoe.mirai.message.data.At
 import net.mamoe.mirai.message.data.PlainText
@@ -44,49 +42,27 @@ object LGZBotCommand: CompositeCommand (
                     PlainText("理由: $reason")
             )
         } catch (e: Exception) {
-            sendMessage("好像...出了点问题... ${e.message}")
+            sendMessage("好像...除了点问题... ${e.message}")
         }
     }
 
-    @SubCommand("unmute")
-    @Description("把某人嘴上的胶布撕下来")
-    suspend fun CommandSender.unmute(user: Member) {
-        try {
-            unmute(user)
-        }
-        catch (e: Exception) {
-            sendMessage("好像...出了点问题... ${e.message}")
-        }
-    }
-
-    @SubCommand("unmute")
-    @Description("把某人嘴上的胶布撕下来")
-    suspend fun CommandSender.unmute(user: Member, group: Group) {
-        try {
-            unmute(user, group)
-        }
-        catch (e: Exception) {
-            sendMessage("好像...出了点问题... ${e.message}")
-        }
-    }
-
-     @SubCommand("atspam")
-     @Description("F**k You!")
-     suspend fun CommandSender.atspam(target: Member, times: Int, sleepTime: Double) {
-         var sleepTime1 = sleepTime
-         var times1 = times
-         if (sleepTime <= .0) {
-             sleepTime1 = 1.0
-         }
-         if (times1 > 10) {
-             times1 = 10
-         }
-         sendMessage("Ok! Processing...")
-         repeat(times1) {
-             delay(round(sleepTime1 * 1000L).toLong())
-             var lastMesg = sendMessage(At(target) + PlainText(RandomUtils.randomText(6)))
-             // delay(100L)
-             lastMesg?.recall()
-         }
-     }
+//     @SubCommand("atspam")
+//     @Description("F**k You!")
+//     suspend fun CommandSender.atspam(target: Member, times: Int, sleepTime: Double) {
+//         var sleepTime1 = sleepTime
+//         if (target.permission.level > 0) {
+//             sendMessage("你不能这么做")
+//             return
+//         }
+//         if (sleepTime <= .0) {
+//             sleepTime1 = 1.0
+//         }
+//         sendMessage("Ok! Processing...")
+//         repeat(times) {
+//             delay(round(sleepTime1 * 1000L).toLong())
+//             var lastMesg = sendMessage(At(target) + PlainText(RandomUtils.randomText(6)))
+//             // delay(100L)
+//             lastMesg?.recall()
+//         }
+//     }
 }
