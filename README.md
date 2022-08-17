65933c7636f266c9989b917e6752df05374d95c0
Xiluo <xiluo@guimc.ltd>
2022-08-17 11:47:36 -0700

    Update README, Make code readable.

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # LGZ-bot
+[![State-of-the-art Shitcode](https://img.shields.io/static/v1?label=State-of-the-art&message=Super%20ShitCode&color=7B5804)](https://github.com/trekhleb/state-of-the-art-shitcode)
 [![FOSSA Status](https://app.fossa.com/api/projects/git%2Bgithub.com%2Fguimc233%2Flgz-bot.svg?type=shield)](https://app.fossa.com/projects/git%2Bgithub.com%2Fguimc233%2Flgz-bot?ref=badge_shield)
 [![Maintainability](https://api.codeclimate.com/v1/badges/05a867056904e1a9f5b1/maintainability)](https://codeclimate.com/github/guimc233/lgz-bot/maintainability)
 [![Codacy Badge](https://app.codacy.com/project/badge/Grade/f4fa5faaba9a42e0bf4213a42ba92a94)](https://www.codacy.com/gh/guimc233/lgz-bot/dashboard?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=guimc233/lgz-bot&amp;utm_campaign=Badge_Grade)
 
 * Thanks for use it!
-* Thanks for @MX233 
+* Thanks for EVERYONE!!!
 
 ## License
 This project is subject to the [GNU Affero General Public License v3.0](LICENSE). This does only apply for source code located directly in this clean repository. During the development and compilation process, additional source code may be used to which we have obtained no rights. Such code is not covered by the GPL license.
--- src/main/kotlin/ltd/guimc/lgzbot/plugin/MessageFilter.kt
+++ src/main/kotlin/ltd/guimc/lgzbot/plugin/MessageFilter.kt
@@ -4,6 +4,7 @@ import ltd.guimc.lgzbot.plugin.PluginMain.adRegex
 import ltd.guimc.lgzbot.plugin.PluginMain.isSuperUser
 import ltd.guimc.lgzbot.plugin.PluginMain.logger
 import ltd.guimc.lgzbot.plugin.files.Config
+import ltd.guimc.lgzbot.plugin.special.CZXTeacher
 import ltd.guimc.lgzbot.plugin.utils.AsciiUtil
 import ltd.guimc.lgzbot.plugin.utils.MessageUtils.getForwardMessage
 import ltd.guimc.lgzbot.plugin.utils.MessageUtils.getPlainText
@@ -32,102 +33,36 @@ object MessageFilter {
 
     private var messagesHandled = 0
     var riskList = ArrayList<Member>()
-    fun checkczx(str :String):Boolean{
-        val unPeekText = AsciiUtil.sbc2dbcCase(str).lowercase()
-            .replace(" ", "")
-            .replace(",", "")
-            .replace(".", "")
-            .replace("!", "")
-            .replace("?", "")
-            .replace(";", "")
-            .replace(":", "")
-            .replace("\"", "")
-            .replace("'", "")
-            .replace("“", "")
-            .replace("”", "")
-            .replace("‘", "")
-            .replace("’", "")
-            .replace("<", "")
-            .replace(">", "")
-            .replace("(", "")
-            .replace(")", "")
-            .replace("內", "内")
-        if (Regex("[Cc][Zz][Xx]|[阝东木辛希]|(ch.*n?z).*[x×]|(陈.*梓)|(梓.*希)").containsMatchIn(unPeekText)) {
-            return true
-        }
-        return false
-    }
+    val cxzTeacher = CZXTeacher()
     suspend fun filter(e: GroupMessageEvent) {
         // 检查权限
 
         val textMessage = e.message.getPlainText()
-                                .removeNonVisible()
+            .removeNonVisible()
         val forwardMessage = e.message.getFullText()
-        val stringLength = if(e.group.id!=912687006L && e.group.id!=735874917L) if (e.sender in riskList) 10 else 35 else 0
-
-        if (forwardMessage.length == 0 && textMessage.length == 0) return
-        val name=PinyinUtils.convertToPinyin(textMessage).lowercase().replace(" ","");
-        if(e.group.id==912687006L) when {
-            (((name.contains("chen") || name.contains("cheng") || name.contains("cen")) &&
-                (name.contains("zi") || name.contains("zhi")) &&
-                (name.contains("xi")) && (name.contains("zixi") || name.contains("zhixi"))) || checkczx(textMessage)) -> {
-                    if(name.contains("chenzixi"))
-                        e.sender.mute(10);
-                    else{
-                        e.sender.mute(65);
-                    }
-                    e.group.sendMessage(PlainText("你先别急，天天陈梓希陈梓希，陈梓希是你爹是吧"))
-                    e.sender.mute(10)
-                    e.message.recall()
-                }
-                else -> {
-                    when {
-                        textMessage.contains("远控") -> {
-                            e.group.sendMessage(PlainText("别急，下一个远控你"))
-                            e.sender.mute(3)
-                        }
-                        else -> {
-                            if (textMessage.contains("余志文")) {
-                                e.group.sendMessage(PlainText("Hacked by Dimples#1337"))
-                                e.sender.mute(3)
-                            }else{
-                                if (textMessage.contains("李佳乐")) {
-                                    e.message.recall()
-                                    e.group.sendMessage(PlainText("天天直呼其名，恶俗狗"))
-                                    e.sender.mute(3)
-                                }else{
-                                    if (textMessage.contains("崩端") || textMessage.contains("崩溃") || (textMessage.contains("fdp") && textMessage.contains("崩"))) {
-                                        e.message.recall()
-                                        e.group.sendMessage(PlainText("fdpcn崩端请看群公告解决，如果不行: \n\nTroubleshooting any problem without the error log is like driving with your eyes closed.\n" +
-                                            "在没有错误日志的情况下诊断任何问题,无异于闭眼开车\n" +
-                                            "   - Apache 官方文档Getting started篇章\n\n日志在 .minecraft/logs"))
-                                        e.sender.mute(3)
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        if(textMessage.lowercase().contains("skiddermc")){
-            e.group.sendMessage(PlainText("关于SkidderMC: 之前有一个外国人加入了UnlegitMC的团队并踢出了所有的原始dev，封锁了仓库并创建了SkidderMC。所以现在的SkidderMC是由外国人谋权篡位所建，其中没有任何一个中国人或是UnlegitMC原始开发人员。特此创建名为UnlegitMinecraft的github组织，并创建FDPCN Client(别名FDP+)Dev均为中国人，原汁原味。\n" +
-                "我们的FDPCN拥有更高的帧率，更好的绕过，更美的UI，感谢大家的选择\n" +
-                "并且SkidderMC辱华，我们也不能保证有没有远控在他们的版本中。\n" +
-                "唯一Github:http://github.com/UnlegitMinecraft/FDPClientChina\n官方网站: https://fdpclient.club/\n" +
-                "QQ群: 912687006"))
+        val stringLength = if (e.group.id == 912687006L && e.group.id == 735874917L) {
+            0
+        } else if (e.sender in riskList) {
+            10
+        } else {
+            35
         }
+
+        if (forwardMessage.isEmpty() && textMessage.isEmpty()) return
+        if (e.group.id == 912687006L) cxzTeacher.specialCheck(e, textMessage)
 
         if (e.sender.permission.level >= e.group.botPermission.level) return
         if ((RegexUtils.matchRegex(adRegex, textMessage) && textMessage.length >= stringLength) ||
-            textMessage.length == 0 && RegexUtils.matchRegex(adRegex, forwardMessage)) {
+            textMessage.isEmpty() && RegexUtils.matchRegex(adRegex, forwardMessage)
+        ) {
             try {
                 e.group.sendMessage(PlainText("嘘~"))
                 e.message.recall()
                 if (!e.sender.permitteeId.hasPermission(isSuperUser)) {
                     e.sender.mute(30)
                 }
+            } catch (_: Exception) {
             }
-            catch (_: Exception) {}
             riskList.add(e.sender)
             setVl(e.sender.id, 99.0)
             messagesHandled++
