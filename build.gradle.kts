8e4af4ac590f6ea7f167b63272b2ca7d9da85789
xiluo <xiluo@guimc.ltd>
2022-08-26 12:16:17 +0800

    Fix some problem, update (Mirai, Kotlin) version, delete a useless library

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    val kotlinVersion = "1.6.10"
+    val kotlinVersion = "1.6.21"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 
-    id("net.mamoe.mirai-console") version "2.12.0"
+    id("net.mamoe.mirai-console") version "2.12.1"
     id("com.github.johnrengelman.shadow") version "1.2.3"
 }
 
@@ -12,7 +12,6 @@ dependencies {
     implementation("com.github.promeg:tinypinyin-lexicons-java-cncity:2.0.3")
     implementation("org.json:json:20220320")
     implementation("org.apache.httpcomponents:httpclient:4.5.13")
-    implementation("org.kohsuke:github-api:1.302")
     implementation("org.jsoup:jsoup:1.15.2")
 }
 
