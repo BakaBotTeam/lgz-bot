4f7b221454c53e2d7e0f1cef358c6679a99f7eee
Xiluo <xiluo@guimc.ltd>
2022-12-21 23:51:14 +0800

    更改权限节点

--- src/main/kotlin/ltd/guimc/lgzbot/PluginMain.kt
+++ src/main/kotlin/ltd/guimc/lgzbot/PluginMain.kt
@@ -76,8 +76,8 @@ object PluginMain : KotlinPlugin(
         bypassMute = register(PermissionId("lgzbot", "bypassmute"), "让某个笨蛋绕过广告禁言")
         blocked = register(PermissionId("lgzbot", "blocked"), "坏蛋专属权限!")
         nudgeMute = register(PermissionId("lgzbot", "nudgemute"), "戳一戳禁言")
-        disableSpamCheck = register(PermissionId("lgzbot.disable", "spamcheck"), "关闭群聊刷屏检查")
-        disableADCheck = register(PermissionId("lgzbot.disable", "spamcheck"), "关闭群聊广告检查")
+        disableSpamCheck = register(PermissionId("lgzbot", "disablespamcheck"), "关闭群聊刷屏检查")
+        disableADCheck = register(PermissionId("lgzbot", "disablespamcheck"), "关闭群聊广告检查")
     }
 
     private fun registerCommands() = CommandManager.run {
