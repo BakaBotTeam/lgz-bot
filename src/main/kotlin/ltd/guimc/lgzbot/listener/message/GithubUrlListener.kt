d069d5a74273e69a994f575dc7b8a44d21b6a750
guimc <xiluo@guimc.ltd>
2023-10-15 01:56:24 +0800

    fix: GithubUrlListener

--- src/main/kotlin/ltd/guimc/lgzbot/listener/message/GithubUrlListener.kt
+++ src/main/kotlin/ltd/guimc/lgzbot/listener/message/GithubUrlListener.kt
@@ -27,14 +27,14 @@ object GithubUrlListener {
 
             val info = GithubUtils.getGithubRepo(GithubUtils.convert(gitLink))
             val s = """[GitHub] (Preview Version)
-Repo: ${info.repo}
-Descriptor: ${info.descriptor}
-Owner: ${info.author.name}
-Default Branch: ${info.defaultBranch}
-Language: ${info.language}
-Last Commit: ${info.lastCommitInfo.commitMessage} (${info.lastCommitInfo.commitId})
-Create Time: ${info.createdTime.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL))}
-Update Time: ${info.updateTime.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL))}"""
+                |Repo: ${info.repo}
+                |Descriptor: ${info.descriptor}
+                |Owner: ${info.author.name}
+                |Default Branch: ${info.defaultBranch}
+                |Language: ${info.language}
+                |Last Commit: ${info.lastCommitInfo.commitMessage} (${info.lastCommitInfo.commitId})
+                |Create Time: ${info.createdTime.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL))}
+                |Update Time: ${info.updateTime.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL))}""".trimMargin()
 
             if (RegexUtils.matchRegex(adRegex, s)) return
             event.group.sendMessage(s)
