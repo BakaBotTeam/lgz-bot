34c5927e6d56460cb4714a63021959e94b95eff0
Guimc <xiluo@guimc.ltd>
2024-01-14 01:09:08 +0800

    reset branch to #62a36ed7

--- src/main/kotlin/ltd/guimc/lgzbot/PluginMain.kt
+++ src/main/kotlin/ltd/guimc/lgzbot/PluginMain.kt
@@ -10,24 +10,32 @@
 package ltd.guimc.lgzbot
 
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runInterruptible
 import ltd.guimc.lgzbot.command.*
 import ltd.guimc.lgzbot.files.Config
 import ltd.guimc.lgzbot.files.GithubSubConfig
+import ltd.guimc.lgzbot.files.GithubWebhookSubData
+import ltd.guimc.lgzbot.files.ModuleStateConfig
 import ltd.guimc.lgzbot.listener.message.FunListener
 import ltd.guimc.lgzbot.listener.message.GithubUrlListener
 import ltd.guimc.lgzbot.listener.message.MessageFilter
 import ltd.guimc.lgzbot.listener.multi.BakaListener
 import ltd.guimc.lgzbot.listener.mute.AutoQuit
 import ltd.guimc.lgzbot.listener.nudge.AntiNudgeSpam
+import ltd.guimc.lgzbot.listener.nudge.NudgeMute
 import ltd.guimc.lgzbot.utils.FbUtils.getFbValue
 import ltd.guimc.lgzbot.utils.RegexUtils.getDefaultPinyinRegex
 import ltd.guimc.lgzbot.utils.RegexUtils.getDefaultRegex
 import ltd.guimc.lgzbot.utils.RequestUtils
+import ltd.guimc.lgzbot.webhook.GithubWebHookReciver
+import ltd.guimc.lgzbot.webhook.WebHookService
 import net.mamoe.mirai.console.command.CommandManager
 import net.mamoe.mirai.console.permission.Permission
 import net.mamoe.mirai.console.permission.PermissionId
 import net.mamoe.mirai.console.permission.PermissionService
-import net.mamoe.mirai.console.plugin.author
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
 import net.mamoe.mirai.console.plugin.name
@@ -38,16 +46,11 @@ import net.mamoe.mirai.event.events.GroupMessageEvent
 import net.mamoe.mirai.event.events.NewFriendRequestEvent
 import net.mamoe.mirai.event.events.NudgeEvent
 import net.mamoe.mirai.event.globalEventChannel
-import kotlin.concurrent.thread
 import java.io.IOException
-import java.nio.file.*
-import kotlin.io.path.*
-import kotlinx.coroutines.*
-import ltd.guimc.lgzbot.files.GithubWebhookSubData
-import ltd.guimc.lgzbot.files.ModuleStateConfig
-import ltd.guimc.lgzbot.listener.nudge.NudgeMute
-import ltd.guimc.lgzbot.webhook.GithubWebHookReciver
-import ltd.guimc.lgzbot.webhook.WebHookService
+import java.nio.file.Path
+import java.nio.file.StandardWatchEventKinds
+import kotlin.concurrent.thread
+import kotlin.io.path.name
 
 object PluginMain : KotlinPlugin(
     JvmPluginDescription(
@@ -69,10 +72,12 @@ object PluginMain : KotlinPlugin(
     lateinit var adPinyinRegex: Array<Regex>
     lateinit var fbValue: Array<String>
     lateinit var webHookService: WebHookService
+    lateinit var configReloadThread: Thread
+    lateinit var configReloadJob: Job
     var isRunning = false
 
     override fun onEnable() {
-        logger.info("$name v$version by $author Loading")
+        logger.info("$name v$version 正在加载喵")
         Config.reload()
         GithubSubConfig.reload()
         ModuleStateConfig.reload()
@@ -87,7 +92,7 @@ object PluginMain : KotlinPlugin(
         registerCommands()
         registerEvents()
         isRunning = true
-        launch {
+        configReloadJob = launch {
             val watchedPath = configFolderPath
             val watcher = try {
                 runInterruptible(Dispatchers.IO) {
@@ -105,7 +110,7 @@ object PluginMain : KotlinPlugin(
             var lastModify = 0L
             var changedFlag = false
 
-            thread {
+            configReloadThread = thread {
                 while (isRunning) {
                     if (changedFlag && System.currentTimeMillis() - lastModify >= 3000) {
                         Config.reload()
@@ -138,14 +143,16 @@ object PluginMain : KotlinPlugin(
             logger.warning("文件监听器已退出")
         }
         webHookService.start()
-        logger.info("$name v$version by $author Loaded")
+        logger.info("$name v$version 加载好了喵")
     }
 
     override fun onDisable() {
+        logger.info("$name v$version 正在卸载喵")
         GithubWebhookSubData.save()
         webHookService.stop()
-        logger.info("$name v$version by $author Disabling")
         isRunning = false
+        configReloadThread.join()
+        logger.info("$name v$version 卸载好了喵")
     }
 
     private fun registerPerms() = PermissionService.INSTANCE.run {
